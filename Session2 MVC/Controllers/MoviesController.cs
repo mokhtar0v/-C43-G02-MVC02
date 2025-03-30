bde066ee8f622e294c13219f37b62c233ef98bfe
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-03-30 16:07:50 +0200

    Routing

---
+++ Session2 MVC/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Session2_MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //action == public, nonstatic method inside the controller
+        //to excute any action => baseurl+controllername+actionname/?
+
+        public string Index(int ID)
+        {
+            return $"Hello, {ID}";
+        }
+        public string GetMovie(int? ID)
+        {
+
+            if(ID is not null)
+            return $"Movie    {ID}";
+
+            return $"No Movie found";
+        }
+    }
+}
--- Session2 MVC/Program.cs
+++ Session2 MVC/Program.cs
@@ -5,9 +5,46 @@ namespace Session2_MVC
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+            app.MapGet("/test", async context => //static segment
+            {
+                await context.Response.WriteAsync("Hello omar");
+            });
+            app.MapGet("/{name}", async context => //variable segment
+            {
+                var name = context.GetRouteValue("name");
+                await context.Response.WriteAsync($"Hello {name}");
+            });
+
+            app.MapGet("/Fixed{name}", async context => //mixed segment
+            {
+                var name = context.Request.RouteValues["name"]; //==var name = context.GetRouteValue("name");
+                await context.Response.WriteAsync($"Hello fixed {name}");
+            });
+
+            app.MapGet("/Movie/GetMovie", async context =>
+            {
+                await context.Response.WriteAsync("Hello GetMovie!");
+            });
+
+            app.MapControllerRoute("default",
+                "{controller}/{action}/{id?}",
+                defaults: new { Controller = "Movies", Action = "Index" });
+
+            app.MapControllerRoute("omarRoute", "{controller = Movies}/{action = GetMovie }/{id}");
+
 
             app.Run();
         }
